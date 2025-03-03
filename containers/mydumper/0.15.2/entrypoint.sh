72471a0b389e5dd0ca8a2649db2e924214c66ad5
Timofey Drozhzhin <26501155+timofey-drozhzhin@users.noreply.github.com>
2025-03-02 21:02:41 -0600

    Add debug info for entrypoint

--- containers/mydumper/0.15.2/entrypoint.sh
+++ containers/mydumper/0.15.2/entrypoint.sh
@@ -5,42 +5,54 @@ set -e
 # Update config
 
 # Ref https://github.com/mydumper/mydumper/blob/master/docs/mydumper_usage.rst
+echo "Configuring mydumper..."
 echo "[mydumper]" > /etc/mydumper.cnf
 
 [[ -n "${MYSQL_HOST}" ]] && \
+  echo "Adding host = ${MYSQL_HOST}" >> /etc/mydumper.cnf
   echo "host = ${MYSQL_HOST}" >> /etc/mydumper.cnf
 
 [[ -n "${MYSQL_PORT}"  ]] && \
+  echo "Adding port = ${MYSQL_PORT}" >> /etc/mydumper.cnf
   echo "port = ${MYSQL_PORT}" >> /etc/mydumper.cnf
 
 [[ -n "${MYSQL_USER}"  ]] && \
+  echo "Adding user = ${MYSQL_USER}" >> /etc/mydumper.cnf
   echo "user = ${MYSQL_USER}" >> /etc/mydumper.cnf
 
 [[ -n "${MYSQL_PASSWORD}"  ]] && \
+  echo "Adding password = ***" >> /etc/mydumper.cnf
   echo "password = ${MYSQL_PASSWORD}" >> /etc/mydumper.cnf
 
 [[ -n "${MYSQL_DATABASE}"  ]] && \
+  echo "Adding database = ${MYSQL_DATABASE}" >> /etc/mydumper.cnf
   echo "database = ${MYSQL_DATABASE}" >> /etc/mydumper.cnf
 
 # The verbosity of messages. 0 = silent, 1 = errors, 2 = warnings, 3 = info. Default is 2.
 [[ -n "${VERBOSE}" ]] && \
+  echo "Adding verbose = ${VERBOSE}" >> /etc/mydumper.cnf
   echo "verbose = ${VERBOSE}" >> /etc/mydumper.cnf
 
 echo "" >> /etc/mydumper.cnf
 
 # Ref https://github.com/mydumper/mydumper/blob/master/docs/myloader_usage.rst
+echo "Configuring myloader..."
 echo "[myloader]" >> /etc/mydumper.cnf  
 
 [[ -n "${MYSQL_HOST}" ]] && \
+  echo "Adding host = ${MYSQL_HOST}" >> /etc/mydumper.cnf
   echo "host = ${MYSQL_HOST}" >> /etc/mydumper.cnf  
 
 [[ -n "${MYSQL_PORT}"  ]] && \
+  echo "Adding port = ${MYSQL_PORT}" >> /etc/mydumper.cnf
   echo "port = ${MYSQL_PORT}" >> /etc/mydumper.cnf  
 
 [[ -n "${MYSQL_USER}"  ]] && \
+  echo "Adding user = ${MYSQL_USER}" >> /etc/mydumper.cnf
   echo "user = ${MYSQL_USER}" >> /etc/mydumper.cnf  
 
 [[ -n "${MYSQL_PASSWORD}"  ]] && \
+  echo "Adding password = ***" >> /etc/mydumper.cnf
   echo "password = ${MYSQL_PASSWORD}" >> /etc/mydumper.cnf  
 
 echo ""
