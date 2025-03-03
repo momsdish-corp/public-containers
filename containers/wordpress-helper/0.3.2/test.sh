adaf0063909bb1b1c55b183e6de1dd659ec658de
Timofey Drozhzhin <26501155+timofey-drozhzhin@users.noreply.github.com>
2025-03-02 20:36:34 -0600

    Check to make sure the entrypoint ran

--- containers/wordpress-helper/0.3.2/test.sh
+++ containers/wordpress-helper/0.3.2/test.sh
@@ -14,3 +14,9 @@ require_cmds() {
 }
 
 require_cmds curl unzip git yq jq htmlq skopeo aws devspace zstd mydumper myloader
+
+# Require the ^host exists in the mydumper.cnf file
+if ! grep -q "^host " /etc/mydumper.cnf; then
+  echo "Required 'host' not found in /etc/mydumper.cnf. Fail!"
+  exit 1
+fi
